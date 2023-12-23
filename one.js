a108f6f35c026618b10ce8e7e5cea0dbf7f5f6be
Noman-Nom <noman5456318@gmail.com>
2023-12-23 11:47:30 +0500

    question no 1 completed

--- one.js
+++ one.js
@@ -1,1 +1,38 @@
-console.log("Hello world");
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
+const createHelloWorld = ()=>{
+
+    const helloWorldReturn = ()=>{
+
+        return "Hello World"
+    }
+
+    return helloWorldReturn
+
+
+
+}
+
+const outputfunction = createHelloWorld()
+
+console.log(outputfunction())
+
+
+//  it is a high order functions 
+
+// Example 1: Create a Repeater Function
+
+const CreateRepeatFunction = (message , repeatCount)=>{
+
+    const repeater = ()=>{
+
+            return message.repeat(repeatCount)
+
+    }
+
+    return repeater
+}
+
+const runCreateRepeatFunction = CreateRepeatFunction("Muhammad Noman" , 3)
+
+console.log(runCreateRepeatFunction())
