a7fb2588e302c41bd7dd67f553ff25ca9a0dc9ef
Noman-Nom <noman5456318@gmail.com>
2023-12-23 12:32:37 +0500

    question2 started

---
+++ two.js
@@ -0,0 +1,8 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc)
+
+// This question is a good reminder of these topics/concepts:
+
+// Closure
+// High-order function
+// Pre-increment vs Post-increment
+
