268ef1eeb40e9e6f5231d55fda60772c03010987
Noman-Nom <noman5456318@gmail.com>
2023-12-25 12:54:35 +0500

    question five started

---
+++ five.js
@@ -0,0 +1,31 @@
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+
+const customMap = (arr , fn)=>{
+
+    const transformedArray = []
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        transformedArray[i]= fn(arr[i], i)
+        
+    }
+    return transformedArray
+}
+
+const mappingFunction = (value , index)=>{
+
+    return value * index
+}
+
+
+
+const resultArray = customMap(arr , mappingFunction)
+
+console.log(resultArray)
