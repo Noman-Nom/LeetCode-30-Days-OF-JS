b6bf577c9b9ffc90c1b0867c8c93c21431674373
Noman-Nom <noman5456318@gmail.com>
2024-07-10 12:12:39 +0500

    problem 9 solved

--- ten.js
+++ ten.js
@@ -15,3 +15,25 @@ const createMultiplier = ( a)=>{   // a = 2
 
 const multiplyBy2 = createMultiplier(2)
 console.log(multiplyBy2(5))
+
+
+
+// Problem 2: Filter Array with Predicate
+// Create a higher-order function filterArray that takes a predicate function and an array as arguments. The returned function should return a new array containing only the elements for which the predicate function returns true.
+
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+const isEven = (num)=>{
+
+        return num %2 ===0
+
+}
+
+        const filterArray = (predicate , arr)=>{
+                return arr.filter(predicate)
+        }
+
+        const evenNumber = filterArray(isEven , arr)
+
+            console.log(evenNumber)
